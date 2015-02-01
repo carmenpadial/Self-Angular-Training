571215526223693876896c3c909a544c7420cd33
cpadial <Carmen.PadialPerez@telefonica.com>
2015-02-01 22:54:42 +0100

    Example4: Defining method on scope

---
+++ example4_definingMethodOnScope/app.js
@@ -0,0 +1,18 @@
+angular.module("app", [])
+    .factory("Data", function() {
+        return {
+            message: "I'm data from a service"
+        }
+    })
+
+    .controller("FirstCtrl", function($scope, Data) {
+        $scope.data = Data;
+    })
+
+    .controller("SecondCtrl", function($scope, Data) {
+        $scope.data = Data;
+
+        $scope.reversedMessage = function(message) {
+            return message.split("").reverse().join("");
+        }
+    })
