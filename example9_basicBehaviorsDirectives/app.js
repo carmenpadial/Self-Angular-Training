b42fe42bd9487826e1f2f778a924dfc72694e8e3
cpadial <Carmen.PadialPerez@telefonica.com>
2015-02-02 21:07:16 +0100

    Example9: Directives: Basic Behaviors

---
+++ example9_basicBehaviorsDirectives/app.js
@@ -0,0 +1,19 @@
+angular.module("behaviors", [])
+
+    // Por defecto el atributo restrict vale "A". Y si solo queremos añadir
+    // comportamiento a nuestra directiva, podemos devolver directamente la función
+
+    .directive("enter", function() {
+        return function(scope, element) {
+            element.bind("mouseenter", function() {
+                console.log("I'm inside!!")
+            })
+        }
+    })
+    .directive("leave", function() {
+        return function(scope, element) {
+            element.bind("mouseleave", function() {
+                console.log("I'm outside!!")
+            })
+        }
+    })
