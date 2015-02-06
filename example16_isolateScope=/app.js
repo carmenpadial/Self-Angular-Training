09ebb61bd4e48ca52de29a8e53c20c3454659fdf
Carmen Padial <cpadial@MALMCPADIAL.local>
2015-02-06 10:30:40 +0100

    Example16: Directives - isolate scope =

---
+++ example16_isolateScope=/app.js
@@ -0,0 +1,14 @@
+angular.module("isolateScope", [])
+
+    .controller("drinkCtrl", function($scope) {
+        $scope.ctrlFlavor = "Soda";
+    })
+
+    .directive("drink", function() {
+        return {
+            scope:{
+                flavor:'=' //Pass this as a string into the scope and assign it. Then you have access to it on the scope inside of your directive
+            },
+            template: '<input type="text" ng-model="flavor"/>'
+        }
+    })
