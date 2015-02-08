50610495bbceec0b21a84bbfccbb382a659987c5
cpadial <Carmen.PadialPerez@telefonica.com>
2015-02-08 21:34:28 +0100

    Example20: building zippy

---
+++ example20_buildingZippy/app.js
@@ -0,0 +1,20 @@
+angular.module("ngApp", [])
+    .directive("zippy", function() {
+        return {
+            restrict: 'E',
+            transclude:true,
+            scope: {
+              title:"@"
+            },
+            template: '<div>' +
+                            '<h3 ng-click="toggleContent()">{{title}}</h3>' +
+                            '<div ng-show="isContentVisible" ng-transclude></div>' +
+                      '</div>',
+            link: function(scope){
+                scope.isContentVisible = false;
+                scope.toggleContent = function() {
+                    scope.isContentVisible = !scope.isContentVisible;
+                }
+            }
+        }
+    })
