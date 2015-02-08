0ec789b2cb4a2c98b073b63b44277b71bdb2860a
cpadial <Carmen.PadialPerez@telefonica.com>
2015-02-08 19:06:45 +0100

    Example17: Directives - isolate scope &

--- example17_isolateScope/app.js
+++ example17_isolateScope/app.js
@@ -1,7 +1,18 @@
-angular.module("ngApp", [])
-    .controller("appCtrl", function($scope) {
-        $scope.callHome = function() {
-            alert("call home!!");
+angular.module("isolateScope", [])
+    .controller("appCtrl", function ($scope) {
+        $scope.callHome = function (message) {
+            alert(message);
         }
 
-})
+    })
+    .directive("phone", function () {
+        return {
+            scope: {
+                dial: '&'
+            },
+            controller: function($scope) {
+              $scope.msg = 'Hello!!'
+            },
+            template: '<input type="text" ng-model="msg"/> <div class="button" ng-click="dial({message:msg})">Call me!!</div>'
+        }
+    })
