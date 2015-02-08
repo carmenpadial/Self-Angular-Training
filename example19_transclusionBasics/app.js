5d33f4de496db7a6489e7e8549bf31cfdc626066
cpadial <Carmen.PadialPerez@telefonica.com>
2015-02-08 20:57:14 +0100

    Example19: Transclusion basics

---
+++ example19_transclusionBasics/app.js
@@ -0,0 +1,16 @@
+angular.module("ngApp", [])
+    .directive('pane', function(){
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: { title:'@' },
+            template: '<div style="border: 1px solid black;">' +
+                '<div style="background-color: gray">{{title}}</div>' +
+                '<ng-transclude></ng-transclude>' +
+                '</div>'
+        };
+    })
+    .controller('ExampleController', ['$scope', function($scope) {
+        $scope.title = 'Lorem Ipsum';
+        $scope.text = 'Neque porro quisquam est qui dolorem ipsum quia dolor...';
+    }]);
