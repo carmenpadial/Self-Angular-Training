d6ad65e5e03fbbe08989cdd51049579f8f623666
cpadial <Carmen.PadialPerez@telefonica.com>
2015-02-10 23:05:45 +0100

    Example22: Zippy app with ng-template

---
+++ example22_templateUrl/app.js
@@ -0,0 +1,17 @@
+angular.module("ngApp", [])
+    .directive("zippy", function() {
+        return {
+            restrict: 'E',
+            transclude:true,
+            scope: {
+                title:"@"
+            },
+            templateUrl: 'zippy.html',
+            link: function(scope){
+                scope.isContentVisible = false;
+                scope.toggleContent = function() {
+                    scope.isContentVisible = !scope.isContentVisible;
+                }
+            }
+        }
+    })
