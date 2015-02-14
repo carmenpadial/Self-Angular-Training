bf196300bcff4f2e6ece3b11fdefe5288adab1b1
cpadial <Carmen.PadialPerez@telefonica.com>
2015-02-14 12:18:07 +0100

    Example32: Route life cycle

--- example32_routeLifeCycle/app.js
+++ example32_routeLifeCycle/app.js
@@ -8,7 +8,7 @@ var app = angular.module('ngApp', ['ngRoute']);
            })
            .when('/new', {
                templateUrl : 'new.html',
-               controller: 'NewCtrl',
+               controller: 'NewCtrl',   // Este controller no sera instanciado hasta que no se resuelva la promesa que va en el atributo resolve
                resolve: {
                    loadData: viewCtrl.loadData
                }
@@ -25,11 +25,14 @@ var app = angular.module('ngApp', ['ngRoute']);
         });
     });
 
-    var viewCtrl = app.controller('ViewCtrl', function($rootScope) {
-
+    var viewCtrl = app.controller('ViewCtrl', function($scope, $location) {
+        $scope.changeRoute = function() {
+            $location.path("/new");
+        }
     });
 
     viewCtrl.loadData = function($q, $timeout) {
+        debugger;
         var defer = $q.defer();
         $timeout(function() {
             defer.resolve({message:"test"});
@@ -37,6 +40,7 @@ var app = angular.module('ngApp', ['ngRoute']);
         return defer.promise;
     }
 
-    app.controller('NewCtrl', function($scope) {
+    app.controller('NewCtrl', function($scope, loadData) { //loadData es el nombre de la promesa declarada en el config
         $scope.data = 'Hello world';
+        console.log("loadData ", loadData);
     });
